a5694080933be79283e8c1b983eb6feb494b39c6
Meredith Howard <mhoward@roomag.org>
2014-07-10 21:56:12 -0400

    init_code_size isn't really useful, dropping it. add decompressor tests

---
+++ t/04_decompressor.rb
@@ -0,0 +1,50 @@
+require "minitest/autorun"
+
+require "lzw"
+ 
+require_relative 'testdata'
+
+describe LZW::Decompressor do
+  it "can be created with no arguments" do
+    LZW::Decompressor.new.must_be_instance_of LZW::Decompressor
+  end
+
+  it "can be created with all arguments" do
+    LZW::Decompressor.new(
+      big_endian:     true,
+    ).must_be_instance_of LZW::Decompressor
+  end
+
+  it "responds to decompress and reset" do
+    d = LZW::Decompressor.new
+    %w( decompress reset ).each { |m| d.must_respond_to m }
+  end
+
+  it "has accessors" do
+    d = LZW::Decompressor.new
+    %w(
+      big_endian
+    ).each { |m| d.must_respond_to m }
+  end
+
+  it "decompresses simple data" do
+    LZW::Decompressor.new.decompress(
+      LZW::Simple.new.compress( LOREM )
+    ).must_equal LOREM
+  end
+
+  it "decompresses big data" do
+    LZW::Decompressor.new.decompress(
+      LZW::Simple.new.compress( BIG )
+    ).length.must_be :<, BIG.length
+  end
+
+  it "decompresses at a limited code size" do
+    LZW::Decompressor.new(
+    ).decompress(
+      LZW::Compressor.new(
+        max_code_size:  9
+      ).compress( LOREM )
+    ).must_equal LOREM
+  end
+end
